2df2d421ebe9dab40f07cce3efee1d249f2b4225
DakotaBuckner07 <61479565+DakotaBuckner07@users.noreply.github.com>
2021-01-13 13:41:21 -0700

    Initial Commit
    Initial commit for the assignment

---
+++ Assets/Scripts/Factory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+    [SerializeField]GameObject prefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            //GameObject gameObject = Instantiate();
+        }
+    }
+}
